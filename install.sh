83a4df0425df0feabb86561e578617d73fbb20ad
Manisha <manisha@shippable.com>
2016-05-20 15:31:10 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,6 @@
 echo "================= Updating global nodejs packages ==================="
 sudo npm install -g grunt-cli mocha vows phantomjs casperjs;
 
-echo "================ Installing selenium =================="
-sudo mkdir -p /srv;
-cd /srv && sudo wget http://selenium-release.storage.googleapis.com/2.48/selenium-server-standalone-2.48.2.jar;
-
 for file in /u14nod/version/*;
 do
   $file
